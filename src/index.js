f9c647b9c67385ec95d81072cd2b1de29d785a28
NicolasGil <nick.gil360@gmail.com>
2022-12-07 16:52:33 -0500

    change font, restructure files

--- src/index.js
+++ src/index.js
@@ -113,13 +113,13 @@ class Game extends React.Component {
 
     return (
       <div className="game">
-        <div className="game-board">
+        <div className="game__board">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
-        <div className="game-info">
+        <div className="game__info">
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
